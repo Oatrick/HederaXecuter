e73156b4b41376e3cb27ea39e87484e5accee004
Calvin Cheng <linchuan.cheng@gmail.com>
2019-05-08 22:41:21 +0800

    webpack for Payment Server. TODO: integrate with Ye Lin's Dockerfile

---
+++ webpack.common.config.babel.js
@@ -0,0 +1,42 @@
+import path from 'path'
+import nodeExternals from 'webpack-node-externals'
+import CopyWebpackPlugin from 'copy-webpack-plugin'
+
+export default {
+    entry: {
+        app: './app.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/',
+        filename: '[name].js',
+        libraryTarget: 'umd'
+    },
+    target: 'node',
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new CopyWebpackPlugin([
+            {
+                from: 'src/index.html',
+                to: 'index.html',
+                toType: 'file'
+            }
+        ])
+    ],
+    node: {
+        // Need this when working with express, otherwise the build fails
+        __dirname: false, // if you don't put this in, __dirname
+        __filename: false // and __filename return blank or /
+    },
+    externals: [nodeExternals()]
+}
--- webpack.development.config.babel.js
+++ webpack.development.config.babel.js
@@ -1,48 +1,16 @@
-import path from 'path'
-import nodeExternals from 'webpack-node-externals'
+import common from './webpack.common.config.babel'
+import merge from 'webpack-merge'
 import WebpackShellPlugin from 'webpack-shell-plugin'
-import CopyWebpackPlugin from 'copy-webpack-plugin'
 
-export default {
-    entry: {
-        app: './app.js'
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '/',
-        filename: '[name].js',
-        libraryTarget: 'umd'
-    },
-    watch: true,
+let developmentConfig = {
     mode: 'development',
-    target: 'node',
-    module: {
-        rules: [
-            {
-                test: /\.js?$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            }
-        ]
-    },
+    devtool: 'source-map',
+    watch: true,
     plugins: [
-        new CopyWebpackPlugin([
-            {
-                from: 'src/index.html',
-                to: 'index.html',
-                toType: 'file'
-            }
-        ]),
         new WebpackShellPlugin({
             onBuildEnd: ['npm run dev:nodemon']
         })
-    ],
-    node: {
-        // Need this when working with express, otherwise the build fails
-        __dirname: false, // if you don't put this in, __dirname
-        __filename: false // and __filename return blank or /
-    },
-    externals: [nodeExternals()]
+    ]
 }
+
+export default merge(common, developmentConfig)
