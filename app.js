923f3f4af04b98fe9a514c524950631efd71a984
Ye Lin Aung <ylaung@zwenex.com>
2019-05-11 13:20:58 +0630

    Changed Port to read from env

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-const PORT = 8099 // Payment Server's default express/socketio server will be 8099
+let PORT = 8099 // Payment Server's default express/socketio server will be 8099
 
 // Not using sticky session
 // import server from './src/payment-server'
 // server.listen(PORT)
 
 // Using sticky session
+if(process.env.PORT != undefined){
+  PORT = process.env.PORT
+}
 import server from './src/payment-server-sticky'
 import sticky from 'sticky-session'
 sticky.listen(server, PORT)
