cce3a413a471de96bd8f36f243435b0dfa5b3148
Calvin Cheng <linchuan.cheng@gmail.com>
2019-05-12 19:11:46 +0800

    Switch to using environment variables to declare our PUBLISHER and PORTAL configuration and secrets

--- src/payment-server-sticky.js
+++ src/payment-server-sticky.js
@@ -8,26 +8,30 @@ import createServer from './create-server'
 
 let server
 
-let REDIS_HOST= process.env.REDIS_HOST
+// TODO: we can switch out to using config and ENV_NAME to retrieve our REDIS_HOST and REDIS_PASSWORD from our dot env file
+// import config from './config'
+// let REDIS_HOST = config[ENV_NAME]['REDIS_HOST']
+// let REDIS_PASSWORD = config[ENV_NAME]['REDIS_PASSWORD']
+let REDIS_HOST = process.env.REDIS_HOST
 let REDIS_PASSWORD = process.env.REDIS_PASSWORD
 
-if(REDIS_HOST == undefined){
-  REDIS_HOST = "localhost"
+if (REDIS_HOST == undefined) {
+    REDIS_HOST = 'localhost'
 }
 if (cluster.isMaster) {
-  server = http.createServer()
-  const io = ioServer().listen(server)
-  
-  const redisServer = redis({ host: REDIS_HOST, port: 6379 })
-  io.adapter(redisServer)
+    server = http.createServer()
+    const io = ioServer().listen(server)
 
-  cluster.on('exit', (worker, code, signal) => {
-    console.log('worker ' + worker.process.pid + ' died')
-  })
+    const redisServer = redis({ host: REDIS_HOST, port: 6379 })
+    io.adapter(redisServer)
+
+    cluster.on('exit', (worker, code, signal) => {
+        console.log('worker ' + worker.process.pid + ' died')
+    })
 }
 
 if (cluster.isWorker) {
-  server = createServer()
+    server = createServer()
 }
 
 export default server
